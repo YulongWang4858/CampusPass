e180003654624e4fdac3c2af442a43d504b1417f
YulongWang4858 <wyl1004858369@gmail.com>
2018-02-22 14:46:03 +0800

    Layout swithcing complete

---
+++ app/src/main/java/com/example/wangyulong/campuspass/ClickListener.java
@@ -0,0 +1,9 @@
+package com.example.wangyulong.campuspass;
+
+/**
+ * Created by wangyulong on 21/02/18.
+ */
+
+public interface ClickListener
+{
+}
---
+++ app/src/main/java/com/example/wangyulong/campuspass/ViewModel/RegisterViewModel.java
@@ -0,0 +1,9 @@
+package com.example.wangyulong.campuspass.ViewModel;
+
+/**
+ * Created by wangyulong on 22/02/18.
+ */
+
+public class RegisterViewModel
+{
+}
