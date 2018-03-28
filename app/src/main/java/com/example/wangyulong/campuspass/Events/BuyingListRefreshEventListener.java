ef0702eae1173b3e9c55db328f0bd019385a35e1
YulongWang4858 <wyl1004858369@gmail.com>
2018-03-28 15:42:37 +0800

    1. request list and item list now refresh upon adding new entries 2. requestlist will not have duplicated entries upon resuming activity

---
+++ app/src/main/java/com/example/wangyulong/campuspass/Events/BuyingListRefreshEventListener.java
@@ -0,0 +1,9 @@
+package com.example.wangyulong.campuspass.Events;
+
+/**
+ * Created by wangyulong on 28/3/18.
+ */
+
+public interface BuyingListRefreshEventListener
+{
+}
---
+++ app/src/main/java/com/example/wangyulong/campuspass/Events/RequestListRefreshEventListener.java
@@ -0,0 +1,10 @@
+package com.example.wangyulong.campuspass;
+
+/**
+ * Created by wangyulong on 25/3/18.
+ */
+
+public interface RequestListRefreshEventListener
+{
+    void onRequestListRefreshEventTrigger();
+}
